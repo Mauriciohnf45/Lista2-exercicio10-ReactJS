06d33fb0b6edbb02d3a323ea94a1d8082cf31176
Mauriciohnf45 <mh173509@gmail.com>
2024-10-19 22:39:04 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Cabecalhos from './components/Cabecalhos'; // Importando o novo componente
+
+function App() {
+  return (
+    <div>
+      <Cabecalhos /> {/* Incluindo o componente Cabecalhos */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Cabecalhos.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Cabecalhos() {
+  return (
+    <React.Fragment>
+      <h3>Primeiro Cabeçalho</h3>
+      <h3>Segundo Cabeçalho</h3>
+      <p>Este é um parágrafo que acompanha os cabeçalhos.</p>
+    </React.Fragment>
+  );
+}
+
+export default Cabecalhos;
